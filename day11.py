29038657bec1b1697e0ef07d12d0e7ed481f7c9f
Siva <sivanagavenkatadineshy@gmail.com>
2026-07-29 20:57:42 +0530

    Add files via upload

---
+++ day11.py
@@ -0,0 +1,225 @@
+# import time
+# t1=()
+# print(t1)
+# print()
+# print(type(t1))
+# t2 = ()
+# print(t2)
+# print()
+# print(type(t2))
+# time.sleep(2)
+
+# print("end of the program")
+
+
+
+# import time
+# t1=(1001,)
+# print(t1)
+# print(type(t1))
+# t2=(1003)
+# print(t1)
+# print(type(t2))
+# time.sleep(2)
+# print("end of the program")
+
+# import time
+# t1 = (1,2,3,4,5,6)
+# print(t1)
+# print(type(t1))
+# print()
+# time.sleep(2)
+# print("end of the program")
+
+
+
+# import time
+# t1 = (1001,1002,1003,1004,1005)
+# print(t1)
+# print(type(t1))
+# print()
+# time.sleep(2)
+# print("end of the program")
+
+
+# import time
+# t1 = "A","B","C"
+# print(t1)
+# print(type(t1))
+# print()
+# time.sleep(2)
+# print("end of the program")
+
+
+# import time 
+# t1 = eval(input("enter the tuple data:"))
+# print(t1)
+# print(type(t1))
+# print("--featching tuple data ----")
+# for x in t1:
+#     print(x)
+# print()
+# time.sleep(2)
+# print("end of the application")
+
+
+
+# import time
+# t1 = (1001,"siva",200000.0,"HCL")
+# print(t1)
+# print()
+# print(type(t1))
+# print("AFTER FEATCHING TUPLE DATA")
+# for x in t1:
+#     print(x)
+# time.sleep(2)
+# print()
+# print("end of the application")
+
+
+
+# import time
+# t1 = (1,2,3,4,5,6,7,8,9)
+# print("before operation")
+# print(t1)
+# print(type(t1))
+# print("after operation")
+# t1[0]=22
+# t1[1]=33
+# t1[2]=44
+# print(t1)
+# print()
+# time.sleep(2)
+# print("end of the appliaction")
+
+
+# import time
+# t1 = (1001,2002,3003,4002,111,423,3333,7777)
+# print(t1)
+# print()
+# print(type(t1))
+# result1=max(t1)
+# # result2=min(t1)
+# # result3=t1.index(7777)
+# # print("max value is :",result1,"min value is :",result2,"iindex of 7777 is :",result3,sep="\n")
+# # time.sleep(2)
+# # print("end of the application")
+
+
+# import time
+# t1 = [1,2,4,5,6,7,7,8,8,543,425,3]
+# print("before opration")
+# print(t1)
+# print()
+# print(type(t1))
+# t2= t1.copy()
+# print(t2)
+# time.sleep(2)
+# print("end of the application")
+
+
+# import time
+# t1 =()
+# print("befor operation")
+# print(t1)
+# print(type(t1))
+# print("After opertaion")
+# t1.append(22)
+# t1.append(33)
+# print(t1)
+# time.sleep(2)
+# print("end of the program")
+
+
+# import time
+# t1 =(1,3,4,3,4,5,3,5,5,4,65,65,99)
+# print("before operation:",t1)
+# print()
+# t1.remove(99)
+# print("after operation:",t1)
+# time.sleep(2)
+# print("end of the application")
+
+# import time
+# t1 = (1001,"Mobile_1",31000,"Samsung")
+# print("before indexing and sclicing :",t1)
+# print(type(t1))
+# print("After operation")
+# print("pid is:",t1[0])
+# print("product is :",t1[1])
+# print("price is :",t1[2])
+# print("company name is :",t1[3])
+# print("using slicing ")
+# print(t1[0])
+# print(t1[::-1])
+# time.sleep(2)
+# print("end of the application")
+
+# Write a program to:
+
+# Take name as input
+# Take age as input
+# Print both
+
+# Example:
+
+# Name : Siva
+# Age : 22
+# name = input("enter ur name:")
+# age = int(input("enter ur age :"))
+# print("name is :",name)
+# print("age is :",age)
+
+# Create a list containing 3 fruits and add one more fruit using append().
+# fruits =["apple","mango","bananna"]
+# fruits.append("orange")
+# print(fruits)
+
+# tuple=(101, "Siva", 85.5)
+# print(tuple[0])
+# print(tuple[-1])
+
+# import time
+# s1 = {}
+# print(s1)
+# print(type(s1))
+# s2=set()
+# print(s2)
+# print(type(s1))
+# time.sleep(2)
+# print("end of the application")
+
+# import time
+# s1 = {1001}
+# print(s1)
+# print(type(s1))
+# print()
+# time.sleep(2)
+# print("end of the application")
+
+
+
+# import time 
+# S1=eval(input("Enter the Set_data:"))
+# print(S1)
+# print()
+# print(type(S1))
+# print()
+# print("====Fetching_Set_Data======")
+# for a1 in S1:
+#     print(a1)
+# print()
+# time.sleep(2)
+# print("End of an application")
+
+
+# import time
+# s1 = {"a","b","c",22,"d"}
+# print(s1)
+# print(type(s1))
+# print("after operation")
+# s1.remove("a")
+# s1.add("k")
+# s1.remove(22)
+# s1.add("dinesh")
+# print("updated set data is :",s1)
