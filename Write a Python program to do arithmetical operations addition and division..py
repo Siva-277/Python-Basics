4f34daf35edf47c50b5240013d9d03d4333ee5c5
Siva <sivanagavenkatadineshy@gmail.com>
2026-07-31 11:23:46 +0530

    Add files via upload

---
+++ Write a Python program to do arithmetical operations addition and division..py
@@ -0,0 +1,16 @@
+#addition
+
+
+num_1 =  eval(input("enter first number:"))
+num_2 = eval(input("enter second number:"))
+
+result = num_1 + num_2
+print(f"sum :{num_1} + {num_2}= {result}")
+
+
+#division
+
+num_3 = eval(input("Enter a number :"))
+num_4 = eval(input("Enter a number :"))
+result_1 =  num_3 / num_4
+print(f"{num_3}/ {num_4}= {result_1}")
