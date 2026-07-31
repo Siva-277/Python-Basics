d321700e919ef1ce21419df9844ee11b0c0ce8dc
Siva <sivanagavenkatadineshy@gmail.com>
2026-07-31 11:30:53 +0530

    Add files via upload

---
+++ WAP to swap 2 variables.py
@@ -0,0 +1,10 @@
+a = input("enter  first variable:")
+b = input("enter second variable:")
+print("before swapping varibale")
+print("a = ",a)
+print("b = ", b)
+a , b = b, a 
+
+print("after swapping variables:")
+print("a =",a)
+print("b =",b)
