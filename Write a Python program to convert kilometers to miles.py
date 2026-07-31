989d59b01124643cad6671e4ba35c4ceee8f07a6
Siva <sivanagavenkatadineshy@gmail.com>
2026-07-31 11:42:47 +0530

    Add files via upload

---
+++ Write a Python program to convert kilometers to miles.py
@@ -0,0 +1,4 @@
+kilometers =  float(input("enter how many kilometers:"))
+conversion_factor = 0.621371
+miles = kilometers *conversion_factor
+print(kilometers,"kilometers equal to the ",miles,"miles")
