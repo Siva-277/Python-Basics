113cc33ddbfaea8dab97405a248cc509c9f73c0a
Siva <sivanagavenkatadineshy@gmail.com>
2026-08-01 23:22:12 +0530

    Update Write a Python Program to Check Armstrong Number.py

--- Write a Python Program to Check Armstrong Number.py
+++ Write a Python Program to Check Armstrong Number.py
@@ -9,3 +9,19 @@ if sum == orginal:
     print("given number is armstrong number")
 else:
     print("given number is not a armstrong number")
+
+
+
+#Write a Python Program to Find Armstrong Number in an Interval.
+start =  int(input("enter starting number:"))
+end = int(input("enter ending number:"))
+for num in range(start , end+1):
+    temp = num
+    digits =  len(str(num))
+    total = 0 
+    while temp > 0 :
+        digit = temp % 10
+        total =  total + digit ** digits
+        temp = temp//10
+    if total ==  num:
+        print(num)
