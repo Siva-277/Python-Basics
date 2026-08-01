962a1708f19dab6c8f83f0d9fbb83f5eb6641477
Siva <sivanagavenkatadineshy@gmail.com>
2026-08-01 22:55:53 +0530

    Enhance prime number check with range output
    Added functionality to print prime numbers from 1 to 100.

--- check give number is prime or not.py
+++ check give number is prime or not.py
@@ -9,3 +9,17 @@ if count==2:
     print(n,"prime numbers")
 else:
     print(n,"not prime number")
+
+
+
+
+#print prime numbers from 1 to 100
+
+print("prime  numbers from 1 to 100")
+for i in range(1,101):
+    count = 0
+    for j in range(1,i+1):
+        if i % j == 0 :
+            count +=1
+    if count == 2:
+        print(i)
