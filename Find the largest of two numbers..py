a62acf1fa1ba0b2f9a25e1566a7762a28edc2870
Siva <sivanagavenkatadineshy@gmail.com>
2026-08-04 11:04:06 +0530

    Add files via upload

---
+++ Find the largest of two numbers..py
@@ -0,0 +1,9 @@
+num_1 = int(input("enter first  number :"))
+num_2 = int(input("enter second number:"))
+
+if num_1>num_2:
+    print(num_1,"is the largest number")
+elif num_1<num_2:
+    print(num_2,"is the largest number")
+else:
+    print("both are equal")
